9b8131c3967eafd0333f73135cab8f45473de01c
somewheve <somewheve@gmail.com>
2019-02-10 03:44:02 +0800

    add url files

---
+++ App/requests_info/url.py
@@ -0,0 +1,14 @@
+
+__all__ = ("USER_ID_URL", "PLAYLIST_URL", "SONG_ID_URL", "COMMENTS_URL")
+# 根据昵称搜索个人用户id url https://music.163.com/weapi/cloudsearch/get/web?csrf_token=
+USER_ID_URL = "https://music.163.com/weapi/cloudsearch/get/web?csrf_token="
+
+# 获取个人歌单列表信息url  https://music.163.com/weapi/user/playlist?csrf_token=
+PLAYLIST_URL = "https://music.163.com/weapi/user/playlist?csrf_token="
+
+# 获取歌曲信息url https://music.163.com/#/playlist?id=2483161318
+SONG_ID_URL = "https://music.163.com/#/playlist?id="
+
+# 获取评论 url http://music.163.com/weapi/v1/resource/comments/R_SO_4_
+COMMENTS_URL = "http://music.163.com/weapi/v1/resource/comments/R_SO_4_"
+
